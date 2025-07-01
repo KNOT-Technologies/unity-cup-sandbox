7e47a148b9b6388de5eba60444680a96a44bed72
knot-hussein <hussein@knottech.ai>
2025-07-01 11:19:33 +0200

    vercel configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        format: "es",
+      },
+    },
+  },
+  server: {
+    headers: {
+      "Content-Type": "application/javascript",
+    },
+  },
+});
